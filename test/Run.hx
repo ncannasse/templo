2f5d422a3d7e304860fa2160bd69b92b98628819
ncannasse <ncannasse@82097fbe-6853-0410-a6d3-3b87655ee02d>
2008-08-29 15:44:57 +0000

    added macros, everything should be done
    git-svn-id: http://temploc.googlecode.com/svn/trunk@10 82097fbe-6853-0410-a6d3-3b87655ee02d

--- test/Run.hx
+++ test/Run.hx
@@ -2,7 +2,6 @@ class Run {
 
 	static function main() {
 		mtwin.templo2.Loader.DEBUG = true;
-		mtwin.templo2.Loader.MACROS = null;
 		mtwin.templo2.Loader.TMP_DIR = "";
 		try neko.FileSystem.deleteFile("test.mtt.n") catch( e : Dynamic ) {};
 		var t = new mtwin.templo2.Loader("test.mtt");
