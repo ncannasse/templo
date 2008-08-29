be496f98a342f10a025b43261e67acf7116212c5
ncannasse <ncannasse@82097fbe-6853-0410-a6d3-3b87655ee02d>
2008-08-29 11:48:03 +0000

    almost finished
    git-svn-id: http://temploc.googlecode.com/svn/trunk@9 82097fbe-6853-0410-a6d3-3b87655ee02d

---
+++ test/Run.hx
@@ -0,0 +1,12 @@
+class Run {
+
+	static function main() {
+		mtwin.templo2.Loader.DEBUG = true;
+		mtwin.templo2.Loader.MACROS = null;
+		mtwin.templo2.Loader.TMP_DIR = "";
+		try neko.FileSystem.deleteFile("test.mtt.n") catch( e : Dynamic ) {};
+		var t = new mtwin.templo2.Loader("test.mtt");
+		trace( t.execute({ x : "cou<b>cou</b>" }) );
+	}
+
+}
---
+++ test/run.bat
@@ -0,0 +1,3 @@
+@echo off
+neko run
+pause
