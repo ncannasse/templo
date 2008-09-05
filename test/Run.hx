63f3d89d9b532beaa735a00775c0c7597a49d942
ncannasse <ncannasse@82097fbe-6853-0410-a6d3-3b87655ee02d>
2008-09-05 10:54:18 +0000

    minor fix : chmod after nekotools boot
    git-svn-id: http://temploc.googlecode.com/svn/trunk@22 82097fbe-6853-0410-a6d3-3b87655ee02d

--- test/Run.hx
+++ test/Run.hx
@@ -5,8 +5,11 @@ class Run {
 		var p = new neko.io.Process("nekotools",["boot","temploc2.n"]);
 		var code = p.exitCode();
 		if( code != 0 ) throw "Error while creating bootable executable";
-		var exe = if( neko.Sys.systemName() == "Windows" ) "temploc2.exe" else "temploc2";
+		var sys = neko.Sys.systemName();
+		var exe = if( sys == "Windows" ) "temploc2.exe" else "temploc2";
 		neko.io.File.copy(exe,dir+exe);
+		if( sys != "Windows" )
+			neko.Sys.command("chmod +x "+dir+exe);
 		neko.FileSystem.deleteFile(exe);
 		neko.Lib.println(exe+" is now available in the current directory");
 	}
